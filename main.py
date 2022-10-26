bead5573b84a56c40e839cb18093b6cc1c73dc79
Ivan_prod <ivan_prod@almazov.ru>
2022-10-26 17:44:28 +0300

    fix: add check for commit message

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config')
 
+ORIGINAL_REMOTE_REPO = config['SETTINGS']['original_remote_repo']
+NEW_REMOTE_REPO = config['SETTINGS']['new_remote_repo']
+
 TARGET_BRANCH = config['SETTINGS']['target_branch']
 PATH_TO_DEV_REPO = config['SETTINGS']['path_to_dev_repo']
 PATH_TO_PROD_REPO = config['SETTINGS']['path_to_prod_repo']
@@ -19,9 +22,6 @@ PROD_REPO_EMAIL = config['SETTINGS']['prod_repo_email']
 NEW_NAME = config['SETTINGS']['new_name']
 NEW_EMAIL = config['SETTINGS']['new_email']
 
-ORIGINAL_REMOTE_REPO = config['SETTINGS']['original_remote_repo']
-NEW_REMOTE_REPO = config['SETTINGS']['new_remote_repo']
-
 # repo.heads.development.commit - последний локальный коммит
 
 def get_last_commit(path_to_src_repo, path_to_dest_repo, target_branch, original_name, original_email, new_name, new_email, commit_file):
@@ -32,20 +32,22 @@ def get_last_commit(path_to_src_repo, path_to_dest_repo, target_branch, original
             if branch.name == target_branch:
                 with open(commit_file, 'r') as f:
                     last_commit = f.read()
-                print(branch.commit.message)
                 if branch.commit.hexsha == last_commit:
-                    print(f' Repo {path_to_src_repo} has no changes')
+                    print(f'Repo {path_to_src_repo} has no changes')
                     repo.git.execute(['git', 'pull', ORIGINAL_REMOTE_REPO, '--allow-unrelated-histories'])
                 else:
+                    print(f'There are changes in repo {path_to_src_repo}, new commit: {branch.commit.hexsha}')
                     with open(commit_file, 'w') as f:
                         f.write(branch.commit.hexsha)
-                    print(f'There are changes in repo {path_to_src_repo}, new commit: {branch.commit.hexsha}')
-                    print('Set new COMMITTER and AUTHOR')
-                    change_commiter(path_to_src_repo, original_email, new_name, new_email)
-                    print('git push...')
-                    push_to_different_repo(path_to_src_repo, path_to_dest_repo)
-                    print('Set original COMMITTER and AUTHOR')
-                    change_commiter(path_to_src_repo, new_email, original_name, original_email)   
+                    if f'Merge branch \'{TARGET_BRANCH}\' of' not in branch.commit.message:
+                        print('Set new COMMITTER and AUTHOR')
+                        change_commiter(path_to_src_repo, original_email, new_name, new_email)
+                        print('git push...')
+                        push_to_different_repo(path_to_src_repo, path_to_dest_repo)
+                        print('Set original COMMITTER and AUTHOR')
+                        change_commiter(path_to_src_repo, new_email, original_name, original_email)
+                    else:
+                        print(f'Latest commit in repo {path_to_src_repo} is "Merge" (\'git pull\' command). I won\'t do anything.')  
 
 
 def change_commiter(path_to_repo, old_email, new_name, new_email):
@@ -79,8 +81,14 @@ def push_to_different_repo(path_to_src_repo, path_to_dest_repo):
     git.Repo(path_to_src_repo).git.execute(['git', 'pull', ORIGINAL_REMOTE_REPO, '--allow-unrelated-histories'])
 
 if __name__ == '__main__':
-    schedule.every(10).seconds.do(get_last_commit, path_to_src_repo=PATH_TO_DEV_REPO, path_to_dest_repo=PATH_TO_PROD_REPO, target_branch=TARGET_BRANCH, original_name=DEV_REPO_NAME, original_email=DEV_REPO_EMAIL, new_name=NEW_NAME, new_email=NEW_EMAIL, commit_file='last_commit_dev.txt')
-    schedule.every(15).seconds.do(get_last_commit, path_to_src_repo=PATH_TO_PROD_REPO, path_to_dest_repo=PATH_TO_DEV_REPO, target_branch=TARGET_BRANCH, original_name=PROD_REPO_NAME, original_email=PROD_REPO_EMAIL, new_name=NEW_NAME, new_email=NEW_EMAIL, commit_file='last_commit_prod.txt')
+    schedule.every(10).seconds.do(get_last_commit, path_to_src_repo=PATH_TO_DEV_REPO, 
+                path_to_dest_repo=PATH_TO_PROD_REPO, target_branch=ORIGINAL_REMOTE_REPO  + '/' + TARGET_BRANCH, 
+                original_name=DEV_REPO_NAME, original_email=DEV_REPO_EMAIL, 
+                new_name=NEW_NAME, new_email=NEW_EMAIL, commit_file='last_commit_dev.txt')
+    schedule.every(15).seconds.do(get_last_commit, path_to_src_repo=PATH_TO_PROD_REPO, 
+                path_to_dest_repo=PATH_TO_DEV_REPO, target_branch=ORIGINAL_REMOTE_REPO  + '/' + TARGET_BRANCH, 
+                original_name=PROD_REPO_NAME, original_email=PROD_REPO_EMAIL, 
+                new_name=NEW_NAME, new_email=NEW_EMAIL, commit_file='last_commit_prod.txt')
     while True:
         schedule.run_pending()
         time.sleep(1)
